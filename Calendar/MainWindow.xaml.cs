354496f5a3e3510ce9db27654a216adbbea8c4de
Евгения Булкина <Evgenia15@users.noreply.github.com>
2020-05-15 13:55:06 +0400

    Добавьте файлы проекта.

---
+++ Calendar/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Kalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string start  = TextBoxOsn.Text;
+            string NewDate = string.Format("{0:" + start + "}", Kalendar.SelectedDate);
+            TextBoxPlc.Text = NewDate;
+        }
+    }
+}
